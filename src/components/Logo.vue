09a9d27dbdaa01faf8d57e8477899bd3879989b6
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2020-10-02 11:42:27 +0900

    add vuetify

---
+++ src/components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="logo">
+    <h1 class="title">7 Days to Die; scrapbook</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scope>
+.logo {
+  .title {
+    #font-size: xx-large;
+    font-size: 64px;
+    font-family: 'EB Garamond', serif;
+    font-color: red;
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
-/**
- *  vue.config.js
- *
- *  c.f. https://cli.vuejs.org/config/#vue-config-js
- */
-
 module.exports = {
   productionSourceMap: false,
+  transpileDependencies: [
+    'vuetify',
+  ],
 };
